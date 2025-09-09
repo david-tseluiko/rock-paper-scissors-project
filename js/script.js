cba0189988f457b82a256c8643c360c850d8d8b6
David Tseluiko <david.tseluiko@gmail.com>
2025-09-09 16:01:33 -0500

    Update scores in playRound function

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log(
-            `It's a tie. You both chose ${humanChoice}`
-        );
-    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log(`It's a tie. You both chose ${humanChoice}`);
+        humanScore++;
+        computerScore++;
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
         console.log(
             `You won. You have ${humanChoice} which beats ${computerChoice}`
         );
+        humanScore++;
     } else {
         console.log(
             `You lost. You have ${humanChoice} which loses to ${computerChoice}`
         );
+        computerScore++;
     }
 }
 
