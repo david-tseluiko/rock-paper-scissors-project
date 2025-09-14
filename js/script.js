3fc1feb25f426a9a4dfe03fceb6c64d9ec888c9b
David Tseluiko <david.tseluiko@gmail.com>
2025-09-14 14:51:09 -0500

    Display results using DOM

--- js/script.js
+++ js/script.js
@@ -1,35 +1,30 @@
+const resultsButton = document.querySelector(".results");
 const playingButtons = document.querySelectorAll(".rock, .paper, .scissors");
 
-playingButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    playRound(button.textContent, getComputerChoice());
-  });
+playingButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.textContent, getComputerChoice());
+    });
 });
 
 let humanScore = 0;
 let computerScore = 0;
-
-playRound();
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
     computerChoice = computerChoice.toLowerCase();
 
     if (humanChoice === computerChoice) {
-        console.log(`It's a tie. You both chose ${humanChoice}`);
+        resultsButton.textContent = `It's a tie. You both chose ${humanChoice}`;
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        console.log(
-            `You won. You have ${humanChoice} which beats ${computerChoice}`
-        );
+        resultsButton.textContent = `You won. You have ${humanChoice} which beats ${computerChoice}`;
         humanScore++;
     } else {
-        console.log(
-            `You lost. You have ${humanChoice} which loses to ${computerChoice}`
-        );
+        resultsButton.textContent = `You lost. You have ${humanChoice} which loses to ${computerChoice}`;
         computerScore++;
     }
 }
